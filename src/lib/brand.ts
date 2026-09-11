ba697662efc21e97782c45b863afcd784034f7b8
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-11 15:30:24 -0300

    Add official MyPets brand asset references

---
+++ src/lib/brand.ts
@@ -0,0 +1,9 @@
+export const BRAND = {
+  name: "MyPets",
+  siteUrl: "https://mypets.lat",
+  facePetsUrl: "https://facepets.org",
+  instagramUrl: "https://www.instagram.com/mypets.lat/",
+  facebookUrl: "https://www.facebook.com/mypets.lat",
+  logoUrl: "https://res.cloudinary.com/fnki0ccg/image/upload/v1789145246/Logo_VPT.png",
+  socialBannerUrl: "https://res.cloudinary.com/fnki0ccg/image/upload/v1789145243/Banner_VPT.png",
+} as const;
