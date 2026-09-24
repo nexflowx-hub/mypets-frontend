1026a5790d83664fc44e79883f29f412919b18ed
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-24 11:57:12 -0300

    Expose ebook-to-feed campaign in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -63,6 +63,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     "/causas",
     "/apoiar",
     "/ajudar",
+    "/ajudar/ebooks",
     "/ajudar/petskids",
     "/apoiar/mypets",
     "/preciso-de-apoio",
